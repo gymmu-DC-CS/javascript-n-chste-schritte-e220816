0b94e5488f47f20a781070ff990169b4ff0d0cb0
vincent <e220816@edu.sbl.ch>
2023-11-30 11:24:51 +0100

    aufgabe 4 und 5 fertig

--- script.js
+++ script.js
@@ -12,8 +12,45 @@ export function aufgabe01(args) {
 }
 
 export function aufgabe02(args) {
-  const input = args
-    const result = input.tuUpperCase()
-    return result
+  return args.toUpperCase()
+}
+  
+
+  export function aufgabe03(args) {
+    const input = args
+    let result = 0
+  
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      if (currentElement === "e" || currentElement === "E") {
+      result++
+      }
+    }
+    return result
   }
 
+  export function aufgabe04(args) {
+    const input = args
+    let result = 0
+  
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      if (currentElement === " " && input[i-1] != "-"){
+        result++
+      }
+      
+  }
+    return result + 1
+  }
+
+  export function aufgabe05(args) {
+    const input = args
+    if (input.toLowerCase() === input) {
+      return false
+    } else {
+      return true
+    }
+  }
+    
+
+  
